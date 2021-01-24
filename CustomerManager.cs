5f7bdb7c803e9c235dce87758c08ea11937ca9e9
semakarbav <semakarbav06mail.com>
2021-01-24 20:52:29 +0300

    23 ocak

---
+++ CustomerManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{//İş Sınıfları --Business Classes
+    class CustomerManager
+    {
+        public void Add(Musteri musteri)
+        {
+          
+        }
+    }
+}
